225f0422668f611788704bd2ea35f1cc0b4b7dbb
hophacks2019mock <specops167@gmail.com>
2018-12-27 17:13:11 -0500

    changes

--- js/scroll.js
+++ js/scroll.js
@@ -7,10 +7,9 @@ window.addEventListener("scroll", function (event) {
     var fadeElem = $('.overlay');
     
     fadeElem.each(function () {
-        var $this = $(this), scrollThreshold = 900;
+        var $this = $(this), scrollThreshold = 1480;
         if (scrollPos > scrollThreshold && !transitionActivated) {
             
-            $(this).animate({ 'opacity': '0' }, 1600);
 
             //remove all particles
             //var elements = document.getElementsByClassName("firefly");
@@ -18,29 +17,30 @@ window.addEventListener("scroll", function (event) {
             // elements[i].classList.remove("firefly");
 
             //removed zoomed in background under text
-            var underlayElem = document.getElementById('underlay');
-            underlayElem.parentNode.removeChild(underlayElem);
+            document.getElementById("underlay").setAttribute('src', 'img/pre_background.png');
+            document.getElementById("underlay").setAttribute('class', null);
+            document.getElementById("underlay").setAttribute('id', 'main_image');
+            setTimeout(function () {
+                document.body.scrollTop = 0; // For Safari
+                document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+            }, 500);
+
             
+            $(this).animate({ 'opacity': '0' }, 1600);
             setTimeout(function () {
 
                 //remove overlay element
                 var overlayElem = document.getElementById('overlay');
                 overlayElem.parentNode.removeChild(overlayElem);
                 
-            }, 1600);
-            window.scrollTo(0, 0);
-           
+            }, 1300);
+         
             transitionActivated = true;
         }
 
 
     })
 });
-
-function hover() {
-    document.getElementById("main_image").setAttribute('src', 'img/background.png');
-    //document.getElementById("registration_button").style.display = "hidden";
-}
 
 
 
